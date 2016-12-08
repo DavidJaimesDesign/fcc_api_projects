6e6534dc593e5a3633d21348c828df977e26b6a2
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-08 05:34:06 -0800

    App responds with index.html now on to actual functionality

--- timestamp_microservice/server.js
+++ timestamp_microservice/server.js
@@ -1,8 +1,9 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
 app.get('/', function(req, res) {
-	res.send('Hello World')
+	res.sendFile(path.join(__dirname + '/views/index.html'))
 });
 
 app.listen(3000, function(){
