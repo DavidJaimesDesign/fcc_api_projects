916a48846c1217a3516fa3a130732fd65b5d7c1e
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-08 06:17:12 -0800

    Only have time validation left

--- timestamp_microservice/server.js
+++ timestamp_microservice/server.js
@@ -2,10 +2,29 @@ var express = require('express')
 var path = require('path')
 var app = express()
 
+function verifydate(date){
+	var result = {
+		unix: null,
+		natural: null
+	}
+
+	if(Date.parse(date) > 1){
+		result.unix = Date.parse(date)
+		result.natural = "Date converted"
+		return JSON.stringify(result)
+	}else {	
+		return JSON.stringify(result)
+	} 
+
+}
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/views/index.html'))
 });
 
+app.get('/:timestamp', function(req, res) {
+	var timestamp = req.params.timestamp
+	res.send("Timestamp: " + verifydate(timestamp) )
+});
 app.listen(3000, function(){
 	console.log("example app listening on port 3000")
 })
