cc1284ce866fec2d29d78231bd50a4e12d54d18d
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-09 05:28:10 -0800

    basic requestheader working

--- requestheader_parser/server.js
+++ requestheader_parser/server.js
@@ -1,2 +1,19 @@
 var express = require('express')
 var path    = require('path')
+var app     = express()
+
+function requestheader(){
+ 	var header = {
+			ip: "hacked", 
+			browser: "chrome",
+			OS: "pineapple"
+		}
+	
+	return JSON.stringify(header)
+}
+
+app.get('/api/whoami/', function(req, res) {
+	res.send(requestheader())
+})
+
+app.listen(process.env.PORT || 3000)
