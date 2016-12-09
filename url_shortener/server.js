496638c4eab78bf306fdceb1fd5e86be1531587a
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-09 13:55:48 -0800

    Figured out pseudo code for the app

--- url_shortener/server.js
+++ url_shortener/server.js
@@ -2,11 +2,18 @@ var express = require('express')
 var path    = require('path')
 var app     = express()
 
+function verifyUrl(url){
+	//returns true if valid false otherwise
+}
+
+
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname + '/views/index.html'))
 });
 
 app.get('/:url', function(req, res) {
+	var url = req.params.url
+	var shortenedUrl = {	
 	res.send("URL will be shortened")
 });
 
