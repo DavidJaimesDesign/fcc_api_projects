fdda396e3759d3790d0dafc5ddb0901916f7c708
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-10 07:04:04 -0800

    I can validate the url passed and generate a shortened url

--- url_shortener/server.js
+++ url_shortener/server.js
@@ -1,12 +1,16 @@
 var express  = require('express')
 var path     = require('path')
 var shortid  = require('shortid')
-var validURL = require('valid-url') //this will help us validate the URL
+var validURL = require('valid-url')
 var app      = express()
 
 function IsURL(url) {
-	return false;
- }
+	if (validURL.isUri(url)){
+		return true
+	} else {
+		return false
+	} 
+}
 
 	
 function generateUrl(){
@@ -17,14 +21,17 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname + '/views/index.html'))
 });
 
-app.get('/new/:url', function(req, res) {
-	var url = req.params.url
+app.get('/new/*', function(req, res) {
+	var url = req.params[0]
 	var error = { error : "That isn't a valid URL."}
 	if (IsURL(url)) {
-		res.send("URL will be shortened" + generateUrl())
+		res.send("URL will be shortened")
 	} else {
 		res.send(JSON.stringify(error))
 		}
 });
 
+app.get('/:shorturl', function(req, res) {
+	//call if in database redirect to site else return error
+})
 app.listen(process.env.PORT || 3000)
