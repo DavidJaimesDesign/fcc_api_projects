a1840c93e86d547ae6970d3ca25fb4e02ccfff3f
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-13 14:48:57 -0800

    server basic code still needs jade stuff

--- filemetadata_microservice/server.js
+++ filemetadata_microservice/server.js
@@ -1,1 +1,24 @@
-//server goes here
+var express     = require('express')
+var path        = require('path')
+var MongoClient = require('mongodb').MongoClient
+var routes      = require('./app/routes/index.js')
+var api         = require('./app/api/metadata.js')
+
+var db 
+
+MongoClient.connect("your db here", function (err, database){
+	if (err) throw err;
+	db = database
+	console.log("connected to filemetadata db")
+		
+	db.createCollection("files",{
+		capped:true,
+		size: 5242880
+		max: 5000
+	})
+
+	routes(app, db)
+	api(app, db)
+
+	app.listen(process.env.PORT || 3000)
+})
