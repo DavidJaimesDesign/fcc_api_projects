090ff79a0433196052c6d15b599d320aaeccae9e
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-14 06:10:06 -0800

    Using multer and post path works only need alert of file size left

--- filemetadata_microservice/app/api/metadata.js
+++ filemetadata_microservice/app/api/metadata.js
@@ -1,7 +1,12 @@
 'use-strict'
-
+var multer      = require('multer')
+var upload      = multer({dest: 'uploads/'})
 module.exports = function(app, db) {
 	app.get('/test', function(req ,res){
 		res.send("api is connected to server")
+	})
+	
+	app.post('/', upload.single('file'), function(req, res, next){
+		res.send("file posted?")
 	})
 }
